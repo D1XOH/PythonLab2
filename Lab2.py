268ceabe207ed34175ce1392a291dd2fcf3cf0ab
D1XOH <70796382+D1XOH@users.noreply.github.com>
2022-09-29 23:17:05 +0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,44 @@
+class Pagination:
+    def __init__(self, items=[], pageSize=10):
+        self.items = items
+        self.pageSize = int(pageSize)
+        self.totalPages = (len(items)//pageSize) + \
+                           (1 if len(items) % pageSize else 0)
+        self.currentPage = 1
+    def getItems(self):
+        return self.items
+    def getPageSize(self):
+        return self.pageSize
+    def getCurrentPage(self):
+        return self.currentPage
+    def prevPage(self):
+        if self.currentPage != 1:
+            self.currentPage -= 1
+        return self
+    def nextPage(self):
+        if self.currentPage != self.totalPages:
+            self.currentPage += 1
+        return self
+    def firstPage(self):
+        self.currentPage = 1
+        return self
+    def lastPage(self):
+        self.currentPage = self.totalPages
+        return self
+    def goToPage(self, pageNum):
+        if 1 < int(pageNum) <= self.totalPages:
+            self.currentPage = int(pageNum)
+        elif int(pageNum) <= 1:
+            self.currentPage = 1
+        else:
+            self.currentPage = self.totalPages
+        return self
+    def getVisibleItems(self):
+        return self.items[(self.currentPage-1)*self.pageSize:self.currentPage*self.pageSize]
+alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+p = Pagination(alphabetList, 4)
+print(p.getVisibleItems())  # ["a", "b", "c", "d"]
+p.nextPage()
+print(p.getVisibleItems())  # ['e', 'f', 'g', 'h']
+p.lastPage()
+print(p.getVisibleItems())  # ["y", "z]
